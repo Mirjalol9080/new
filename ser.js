518d860b498112109a9d3003b895473f2a9e9def
Mirjalol9080 <pubgmabile969@gmail.com>
2024-08-05 14:49:38 +0500

    Mirjo

--- ser.js
+++
@@ -1,6 +0,0 @@
-const userCountElement = document.getElementById('user-count');
-const socket = new WebSocket('ws://' + window.location.host);
-
-socket.onmessage = function(event) {
-    userCountElement.textContent = event.data;
-};
